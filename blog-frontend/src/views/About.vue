15f462d56222405e5a4cedde7f5bf55a14c74237
小鹿 <admin@xiaolu.college>
2026-06-13 00:51:38 +0800

    fix: GitHub链接改为 Onlooker92013

--- blog-frontend/src/views/About.vue
+++ blog-frontend/src/views/About.vue
@@ -5,7 +5,7 @@
       <h1>🦌 {{ settings.site_name || '小鹿' }}</h1>
       <p class="about-tagline">{{ settings.site_slogan }}</p>
       <div class="about-social">
-        <a href="https://github.com" target="_blank" rel="noopener" class="social-link" title="GitHub">
+        <a href="https://github.com/Onlooker92013" target="_blank" rel="noopener" class="social-link" title="GitHub">
           <el-icon :size="20"><svg viewBox="0 0 24 24" fill="currentColor"><path d="M12 0a12 12 0 0 0-3.38 23.38c.6.12.82-.26.82-.57l-.01-2.24c-3.34.73-4.04-1.43-4.04-1.43-.55-1.4-1.34-1.77-1.34-1.77-1.1-.74.08-.73.08-.73 1.22.09 1.87 1.25 1.87 1.25 1.08 1.86 2.84 1.3 3.53 1 .11-.8.44-1.3.8-1.6-2.67-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.52.12-3.18 0 0 1.01-.32 3.3 1.23a11.5 11.5 0 0 1 6 0c2.28-1.55 3.28-1.23 3.28-1.23.66 1.66.24 2.88.12 3.18a4.6 4.6 0 0 1 1.24 3.22c0 4.61-2.81 5.63-5.48 5.92.43.37.8 1.1.8 2.22v3.3c0 .31.22.69.82.57A12 12 0 0 0 12 0z"/></svg></el-icon>
         </a>
         <a href="mailto&#58;admin@xiaolu.college" class="social-link" title="Email">
